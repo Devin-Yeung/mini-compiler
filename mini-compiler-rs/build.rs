23420e5c3ba74365dd6e291bbab0fb2f0547150e
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-10-27 10:25:47 +0800

    fix: fix build.rs

--- mini-compiler-rs/build.rs
+++ mini-compiler-rs/build.rs
@@ -1,5 +1,5 @@
 use bindgen;
-use std::env;
+use std::{env, fs};
 use std::path::PathBuf;
 
 use bindgen::CargoCallbacks;
@@ -7,16 +7,16 @@ use bindgen::CargoCallbacks;
 fn main() {
     // This is the directory where the `c` library is located.
     let libdir_path = PathBuf::from(env::var_os("OUT_DIR").unwrap())
-        .join("build")
-        // Canonicalize the path as `rustc-link-search` requires an absolute
-        // path.
-        .canonicalize()
+        .join("build");
+    let _ = fs::create_dir_all(&libdir_path);
+    // Canonicalize the path as `rustc-link-search` requires an absolute path.
+    let libdir_path = libdir_path.canonicalize()
         .expect("cannot canonicalize path");
 
     let mut cfg = cc::Build::new();
     cfg.file("../lexer.c")
         .file("../log.c")
-        .out_dir(libdir_path.clone());
+        .out_dir(&libdir_path);
 
     cfg.compile("compiler");
 
