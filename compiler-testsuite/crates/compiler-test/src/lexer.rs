d71df81baed44e0d5a8c08aa3d8f33b652fded63
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-10-27 23:44:13 +0800

    feat: snapshots for all the lexer examples

--- compiler-testsuite/crates/compiler-test/src/lexer.rs
+++ compiler-testsuite/crates/compiler-test/src/lexer.rs
@@ -1,10 +1,58 @@
-#[cfg(test)]
-mod tests {
-    use mini_compiler_rs::lexer::Lexer;
-    use testsuite::unittest;
+use std::fs;
+use std::path::PathBuf;
 
-    unittest!(example_1, |_, src| {
-        let token = Lexer::new(src).collect::<Vec<_>>();
+macro_rules! path {
+    ($root:expr, $($segment:expr),+) => {{
+        let mut root: PathBuf = $root;
+        $(root.push($segment);)*
+        root
+    }}
+}
+
+fn snap<S: AsRef<str>>(src_name: S) {
+    let root = PathBuf::from(std::env!("CARGO_MANIFEST_DIR"));
+    let source_path = path!(
+        root.clone(),
+        "snapshots",
+        "lexer",
+        "input",
+        src_name.as_ref()
+    );
+    let snapshot_path = path!(root.clone(), "snapshots", "lexer", "output");
+
+    insta::with_settings!({
+        snapshot_path => snapshot_path,
+        prepend_module_to_snapshot => false,
+        snapshot_suffix => src_name.as_ref(),
+    },{
+        let source = fs::read_to_string(source_path).unwrap();
+        let token = mini_compiler_rs::lexer::Lexer::new(&source).collect::<Vec<_>>();
         insta::assert_debug_snapshot!(token);
     });
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer::snap;
+    use std::fs;
+    use std::path::PathBuf;
+
+    #[test]
+    fn lexer_snapshots() {
+        let src_dir = path!(
+            PathBuf::from(std::env!("CARGO_MANIFEST_DIR")),
+            "snapshots",
+            "lexer",
+            "input"
+        );
+
+        for entry in fs::read_dir(src_dir).unwrap() {
+            let path = entry.unwrap().path();
+
+            if path.is_file() {
+                let path = path.file_name().unwrap().to_str().unwrap();
+                snap(path);
+            }
+        }
+    }
 }
