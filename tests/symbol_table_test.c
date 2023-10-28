aeb0f8bc415e85161273e7dd67ac2c1008cdb95a
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-10-28 11:27:01 +0800

    feat: more test cases to test symbol table

--- tests/symbol_table_test.c
+++ tests/symbol_table_test.c
@@ -1,14 +1,31 @@
 #include "../symbol_table.h"
 
 #include <assert.h>
+#include <stdio.h>
+
+SymbolTy gen_ty(unsigned i) {
+    switch (i % 3) {
+        case 0:
+            return FuncTy;
+        case 1:
+            return BoolTy;
+        case 2:
+            return NatTy;
+        default:
+            assert(0);
+    }
+}
 
 int main() {
-    SymbolTable* t = symbol_table_new();
-    symbol_table_insert(t, "hello", FuncTy);
-    symbol_table_insert(t, "world", NatTy);
-    symbol_table_insert(t, "!", BoolTy);
-    assert(symbol_table_find(t, "hello") == FuncTy);
-    assert(symbol_table_find(t, "world") == NatTy);
-    assert(symbol_table_find(t, "!") == BoolTy);
+    SymbolTable *t = symbol_table_new();
+    char buf[64];
+    for (unsigned i = 0; i < 1000; i++) {
+        snprintf(buf, sizeof(buf), "%u", i);
+        symbol_table_insert(t, buf, gen_ty(i));
+    }
+    for (unsigned i = 0; i < 1000; i++) {
+        snprintf(buf, sizeof(buf), "%u", i);
+        assert(symbol_table_find(t, buf) == gen_ty(i));
+    }
     symbol_table_destroy(t);
 }
