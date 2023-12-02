b8eb0a611bb582b11354e073b94d7285a55a8b8f
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-12-02 18:05:06 +0800

    fix: use `==` instead of `=` in test

--- tests/slr_test.c
+++ tests/slr_test.c
@@ -7,8 +7,8 @@ int main() {
     Grammar* grammar = grammar_new();
     SLRParser* parser = slr_parser_init(grammar, &SLR_TABLE);
     SLRop op = goto_table_get(SLR_TABLE.goto_table, 0, 'S');
-    assert(op.value = 1);
-    assert(op.ty = SLR_GOTO);
+    assert(op.value == 1);
+    assert(op.ty == SLR_GOTO);
     op = shift_reduce_table_get(SHIFT_REDUCE_TABLE, 0, Identifier);
     assert(op.value == 0);
     assert(op.ty == SLR_EMPTY);
