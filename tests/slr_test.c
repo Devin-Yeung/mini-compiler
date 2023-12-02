1fb477709cdaa9323569709c47e4b59a7d99bdc6
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-12-02 18:04:16 +0800

    feat: get item from SLR shift-reduce table

--- tests/slr_test.c
+++ tests/slr_test.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdio.h>
 
 #include "../parser.h"
 
@@ -8,6 +9,15 @@ int main() {
     SLRop op = goto_table_get(SLR_TABLE.goto_table, 0, 'S');
     assert(op.value = 1);
     assert(op.ty = SLR_GOTO);
+    op = shift_reduce_table_get(SHIFT_REDUCE_TABLE, 0, Identifier);
+    assert(op.value == 0);
+    assert(op.ty == SLR_EMPTY);
+    op = shift_reduce_table_get(SHIFT_REDUCE_TABLE, 8, Semicolon);
+    assert(op.value == 14);
+    assert(op.ty == SLR_REDUCE);
+    op = shift_reduce_table_get(SHIFT_REDUCE_TABLE, 22, Colon);
+    assert(op.value == 29);
+    assert(op.ty == SLR_SHIFT);
     destroy_slr_parser(parser);
     destroy_grammar(grammar);
 }
