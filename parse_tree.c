7c35ec4a9f090b1262203365354fa0421ed2f041
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-12-15 15:26:18 +0800

    feat: draft API of parse tree

--- parse_tree.c
+++ parse_tree.c
@@ -1,1 +1,31 @@
 #include "parse_tree.h"
+
+ParseTree* parse_tree_init(ParseTreeNode* node) {
+    ParseTree* tree = malloc(sizeof(ParseTree));
+    tree->root;
+    return tree;
+}
+
+ParseTreeNode* parse_tree_node_init(SLRSymbol* sym) {
+    ParseTreeNode* node = malloc(sizeof(ParseTreeNode));
+    node->SLRSymbol = sym;
+    node->children = NULL;
+    return node;
+}
+
+void parse_tree_node_add_first(ParseTreeNode* node, ParseTreeNode* child) {
+    if (node->children == NULL) {
+        cc_deque_new(&node->children);
+    }
+    cc_deque_add_first(node->children, child);
+}
+
+ParseTreeNode* parse_tree_node_remove_last(ParseTreeNode* node) {
+    ParseTreeNode* out;
+    if (node->children == NULL || cc_deque_size(node->children) == 0) {
+        return NULL;
+    } else {
+        cc_deque_remove_last(node->children, (void*)&out);
+        return out;
+    }
+}
--- parse_tree.h
+++ parse_tree.h
@@ -1,4 +1,25 @@
 #ifndef MINI_COMPILER_PARSE_TREE_H
 #define MINI_COMPILER_PARSE_TREE_H
 
-#endif //MINI_COMPILER_PARSE_TREE_H
+#include "parser.h"
+
+typedef enum NodeTy {
+    NODE_TERMINAL,
+    NODE_NON_TERMINAL,
+} NodeTy;
+
+typedef struct ParseTreeNode {
+    SLRSymbol* SLRSymbol;
+    CC_Deque* children;
+} ParseTreeNode;
+
+typedef struct ParseTree {
+    ParseTreeNode* root;
+} ParseTree;
+
+ParseTree* parse_tree_init(ParseTreeNode* node);
+ParseTreeNode* parse_tree_node_init(SLRSymbol* sym);
+void parse_tree_node_add_first(ParseTreeNode* node, ParseTreeNode* child);
+ParseTreeNode* parse_tree_node_remove_last(ParseTreeNode* node);
+
+#endif  // MINI_COMPILER_PARSE_TREE_H
